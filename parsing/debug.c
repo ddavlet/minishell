fb77d2e560d2ae4f7a0e45ac5b93036e2049e0ba
Dilshod Davletyarov <ddavlety@c4b8c2.42berlin.de>
2024-02-17 19:36:02 +0100

    parsing 1.3

---
+++ parsing/debug.c
@@ -0,0 +1,30 @@
+#include "parsing.h"
+#include <stdio.h>
+
+void	debug_print(t_com *com)
+{
+	ft_printf("\nCommand:\n");
+	ft_printf("comand: %s\n", com->com);
+	ft_printf("operation code: %d\n", com->operat);
+	int	i = 0;
+	while ((com->args)[i])
+		ft_printf("arguments: %s\n", (com->args)[i++]);
+}
+
+void	debug_print_come(t_com **commands)
+{
+	size_t	i;
+
+	i = 0;
+	while (commands[i])
+		debug_print(commands[i++]);
+}
+
+void	debug_print_tokens(char **tokens)
+{
+	ssize_t	i;
+
+	i = -1;
+	while (tokens[++i])
+		printf("token %lo: %s\n", i, tokens[i]);
+}
---
+++ parsing/terminating.c
@@ -0,0 +1,28 @@
+#include "parsing.h"
+
+int	terminate_commands(t_com **commands)
+{
+	int	i;
+
+	i = 0;
+	if (commands)
+	{
+		while (commands[i])
+			free(commands[i++]);
+	}
+	free(commands);
+	return (0);
+}
+
+int	terminate_tokens(char **ptr)
+{
+	ssize_t	i;
+
+	i = 0;
+	if (!ptr)
+		return (0); //??
+	while (ptr[i])
+		free(ptr[i]);
+	free(ptr);
+	return (0);
+}
