c364f25e6d7d7043e65776a29a20c0430cbf6257
vketteni <v.ketteniss@outlook.com>
2024-02-18 01:05:57 +0100

    Feb 18 24

--- execution/execution.c
+++ execution/execution.c
@@ -1,7 +1,7 @@
 
 #include "execution.h"
 
-int	execution(t_pars **cmd_arr)
+int	execution(t_pars **cmd_arr, char *envp[])
 {
 	int		i;
 	int		fd[2];
@@ -10,17 +10,12 @@ int	execution(t_pars **cmd_arr)
 	i = 0;
 	while (cmd_arr[i])
 	{
-		if (cmd_arr[i]->operat == PIPE)
+		if (cmd_arr[i]->operat == AND)
 		{
-			pipe(fd);
 			pid = fork();
 			if (pid == 0)
 			{
-				// CHILD
-				dup2(fd[1], 1);
-				close(fd[0]);
-				close(fd[1]);
-				if (execve(cmd_arr[i]->name, cmd_arr[i]->args, NULL) == -1)
+				if (execve(cmd_arr[i]->name, cmd_arr[i]->args, envp) == -1)
 				{
 					perror("execve");
 					exit(1);
@@ -28,8 +23,60 @@ int	execution(t_pars **cmd_arr)
 			}
 			else if (pid > 0)
 			{
+				waitpid(pid, NULL, 0);
+			}
+			else
+			{
+				perror("fork");
+				exit(1);
+			}
+		}
+		else if (cmd_arr[i]->operat == OR)
+		{
+				pid = fork();
+				if (pid == 0)
+				{
+					if (execve(cmd_arr[i]->name, cmd_arr[i]->args, envp) == -1)
+					{
+						perror("execve");
+						exit(1);
+					}
+				}
+				else if (pid > 0)
+				{
+					waitpid(pid, NULL, 0);
+				}
+				else
+				{
+					perror("fork");
+					exit(1);
+				}
+		}
+		else
+		if (cmd_arr[i]->operat == PIPE)
+		{
+			if (pipe(fd) == -1)
+			{
+				perror("pipe");
+				exit(1);
+			}
+			pid = fork();
+			if (pid == 0)
+			{
+				// CHILD
+				dup2(fd[1], 1); // redirect stdout to the write end of the pipe
+				close(fd[0]);
+				close(fd[1]);
+				if (execve(cmd_arr[i]->name, cmd_arr[i]->args, envp) == -1)
+				{
+					perror("execve");
+					exit(1);
+				}
+			}
+			else if (pid > 0) 
+			{
 				// PARENT
-				dup2(fd[0], 0);
+				dup2(fd[0], 0); // redirect stdin to the read end of the pipe
 				close(fd[0]);
 				close(fd[1]);
 				waitpid(pid, NULL, 0);
@@ -45,7 +92,7 @@ int	execution(t_pars **cmd_arr)
 			pid = fork();
 			if (pid == 0)
 			{
-				if (execve(cmd_arr[i]->name, cmd_arr[i]->args, NULL) == -1)
+				if (execve(cmd_arr[i]->name, cmd_arr[i]->args, envp) == -1)
 				{
 					perror("execve");
 					exit(1);
@@ -63,5 +110,18 @@ int	execution(t_pars **cmd_arr)
 		}
 		i++;
 	}
+	return (0);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	// t_pars	**commands = parse_text(argv[1]);
+	t_pars	**commands = parse_text("ls -l | grep .c | wc -l");
+
+	debug_print_parse(commands);
+	execution(commands, envp);
+
+	(void)argc;
+	(void)argv;
 	return (0);
 }
--- execution/execution.h
+++ execution/execution.h
@@ -3,11 +3,12 @@
 # define EXECUTION_H
 
 # include "../parsing/parsing.h"
+# include "../testing/testing.h"
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 
 
-int	execution(t_pars **cmd_arr);
+int	execution(t_pars **cmd_arr, char *envp[]);
 
 #endif
