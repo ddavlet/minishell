ca0ebf2fa6394d3a158316234b2fd9f79b1e92b3
ddavlety <ddavlety@student.42berlin.de>
2024-03-14 16:25:45 +0100

    reverse_pars checked

--- execution/reverse_pars.c
+++ execution/reverse_pars.c
@@ -6,7 +6,7 @@
 /*   By: ddavlety <ddavlety@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:07:44 by ddavlety          #+#    #+#             */
-/*   Updated: 2024/03/14 15:30:55 by ddavlety         ###   ########.fr       */
+/*   Updated: 2024/03/14 16:25:37 by ddavlety         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 char	*operation(t_oper type)
 {
-	char *types[] = {"", "|", "&", "||", "&&"};
+	char	*types[5];
+
+	types[0] = NULL;
+	types[1] = "|";
+	types[2] = "&";
+	types[3] = "||";
+	types[4] = "&&";
+	if (type == RUN)
+		return (NULL);
 	return (ft_strdup(types[type]));
 }
 
 size_t	ft_arr_cpy(char **dst, char **src, size_t *pos)
 {
 	while (*src)
-		dst[*pos++] = ft_strdup(*src++);
+		dst[(*pos)++] = ft_strdup(*src++);
 	return (*pos);
 }
 
@@ -35,13 +43,13 @@ char	**reverse_pars(t_cmd **cmds_tojoin, int commands)
 	len = 0;
 	while (i < commands)
 		len += ft_arr_len(cmds_tojoin[i++]->argv);
-	argv = (char **) ft_calloc(sizeof(char *), len + commands);
+	argv = (char **) ft_calloc(sizeof(char *), len + commands + 1);
 	i = 0;
 	len = 0;
 	while (i < commands)
 	{
 		ft_arr_cpy(argv, cmds_tojoin[i]->argv, &len);
-		argv[len] = operation(cmds_tojoin[i]->operat);
+		argv[len++] = operation(cmds_tojoin[i]->operat);
 		i++;
 	}
 	return (argv);
