adbd94cb4b9b5fa35dc97f3e3e4b5a3526c01ecd
ddavlety <ddavlety@student.42berlin.de>
2024-03-19 17:25:26 +0100

    cd in progress

--- builtin/cd.c
+++ builtin/cd.c
@@ -2,10 +2,10 @@
 
 int	builtin_cd(char **argv, t_env *root)
 {
-	char	*path;
-	DIR		*dir;
-    int		ret;
-
+	char			*path;
+	DIR				*dir;
+	int				ret;
+	struct dirent	*entry;
 
 	if (argv[2])
 		return (builtin_err_gen("cd", "too many arguments", NULL));
@@ -18,12 +18,13 @@ int	builtin_cd(char **argv, t_env *root)
 	else
 		path = argv[1];
 	dir = opendir(path);
+	entry = readdir(dir);
 	if (!dir)
 	{
 		return (builtin_err_gen("cd", "No such file or directory", path));
 	}
 	ret = chdir(path);
 	append_envp(root, "OLDPWD", find_var(root, "PWD"));
-	append_envp(root, "PWD", path);
+	append_envp(root, "PWD", entry->d_name);
 	return (0);
 }
