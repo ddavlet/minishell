114a269b74f5a13c34d81994ff5054aa7f74dc5f
Vincent Ketteniss <vketteni@c4c6c5.42berlin.de>
2024-03-21 18:56:02 +0100

    Mar 21 24

--- execution/utils/execute.c
+++ execution/utils/execute.c
@@ -5,8 +5,13 @@ int	execute(char **argv, char **envp)
 	char	*path;
 
 	if (!argv || !envp)
-		terminate(NULL, EXIT_FAILURE, "execute: parameter check failed");
-	path = get_path(argv[0], envp);
+		terminate(NULL, EXIT_FAILURE, "execute: parameter check failed");\
+	if (*argv[0] == '/')
+		path = argv[0];
+	else if (*argv[0] == '.' || *argv[0] == '~')
+		path = get_relative(argv[0]);
+	else
+		path = get_path(argv[0], envp);
 	if (path == NULL)
 		terminate(NULL, EXIT_FAILURE, "minishell: couldn't find path");
 	if (execve(path, argv, envp) == -1)
