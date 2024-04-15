09813e43691f5ae0672268a222e93ba6ab2e50ab
ddavlety <ddavlety@student.42berlin.de>
2024-04-15 19:29:22 +0200

    in progress (tokinizing)

--- parsing/utils/is_token.c
+++ parsing/utils/is_token.c
@@ -2,15 +2,19 @@
 
 int	passed_shell_operator(int pos, const char *line)
 {
-	if (ft_isalnum(line[pos]) && pos > 0 && line[pos - 1] == '|')
-		return (1);
-	if (ft_isalnum(line[pos]) && pos > 1 && line[pos - 1] == '|' && line[pos
+	if (pos == 2 && line[pos - 1] == '|' && line[pos
 		- 2] == '|')
 		return (1);
-	if (ft_isalnum(line[pos]) && pos > 1 && line[pos - 1] == '&' && line[pos
+	if (pos == 1 && line[pos - 1] == '|')
+		return (1);
+	if (ft_isalnum(line[pos]) && pos == 2 && line[pos - 1] == '&' && line[pos
 		- 2] == '&')
 		return (1);
-    return (0);
+	if (pos > 1 && line[pos] == '&' && line[pos + 1] == '&')
+		return (1);
+	if (pos > 1 && line[pos] == '|' && line[pos + 1] != '|')
+		return (1);
+	return (0);
 }
 
 int	is_between_char(int pos, const char *line, char c)
