18f9dcd567aec0da59c1e7add45d4a6e4a6e59b8
ddavlety <ddavlety@student.42berlin.de>
2024-04-16 15:31:45 +0200

    signals in heardoc fixed

--- main.c
+++ main.c
@@ -7,9 +7,10 @@ int	main(int argc, char *argv[], const char *envp[])
 	t_cmd2	*cmds;
 	t_token	*tokens;
 	t_env	*shell_env;
+	char	*prompt;
 
 	shell_env = initialize_shell(envp);
-	// printf("pid of main: %d\n", getpid());
+	printf("pid of main: %d\n", getpid());
 	while (1)
 	{
 		g_signal = 0;
@@ -18,10 +19,9 @@ int	main(int argc, char *argv[], const char *envp[])
 			tokens = tokenizer(argv[2]);
 		else
 		{
-			const char *prompt = shell_prompt(shell_env);
-			// char *prompt = ft_strdup("ec 412");
+			prompt = shell_prompt(shell_env);
 			tokens = tokenizer(prompt);
-			free((char *)prompt);
+			free(prompt);
 		}
 		cmds = parser(tokens, shell_env);
 		execution(cmds);
