8a04796bef396f2a4cd648b5e673c181ba60917b
ddavlety <ddavlety@student.42berlin.de>
2024-04-16 12:29:54 +0200

    redir and she opers with no spaces fixed

--- parsing/utils/is_token.c
+++ parsing/utils/is_token.c
@@ -2,17 +2,39 @@
 
 int	passed_shell_operator(int pos, const char *line)
 {
-	if (pos == 2 && line[pos - 1] == '|' && line[pos
-		- 2] == '|')
+	if (pos == 1 && line[pos - 1] == '|' && line[pos] != '|')
 		return (1);
-	if (pos == 1 && line[pos - 1] == '|')
+	if (pos == 2 && line[pos - 1] == '|' && line[pos - 2] == '|')
 		return (1);
-	if (ft_isalnum(line[pos]) && pos == 2 && line[pos - 1] == '&' && line[pos
-		- 2] == '&')
+	if (pos == 2 && line[pos - 1] == '&' && line[pos - 2] == '&')
 		return (1);
-	if (pos > 1 && line[pos] == '&' && line[pos + 1] == '&')
+	if (pos > 2 && line[pos] == '&' && line[pos + 1] == '&')
+		return (1);
+	if (pos > 2 && line[pos] == '|' && line[pos + 1] == '|')
 		return (1);
 	if (pos > 1 && line[pos] == '|' && line[pos + 1] != '|')
+		return (1);
+	return (0);
+}
+
+int	passed_redir_sign(int pos, const char *line)
+{
+	if (pos == 1 && line[pos - 1] == '<' && line[pos] != '<')
+		return (1);
+	if (pos == 2 && line[pos - 1] == '<' && line[pos - 2] == '<')
+		return (1);
+	if (pos == 1 && line[pos - 1] == '>' && line[pos] != '>')
+		return (1);
+	if (pos == 2 && line[pos - 1] == '>' && line[pos - 2] == '>')
+		return (1);
+
+	if (pos > 2 && line[pos] == '<' && line[pos + 1] == '<')
+		return (1);
+	if (pos > 1 && line[pos] == '<' && line[pos + 1] != '<')
+		return (1);
+	if (pos > 2 && line[pos] == '>' && line[pos + 1] == '>')
+		return (1);
+	if (pos > 1 && line[pos] == '>' && line[pos + 1] != '>')
 		return (1);
 	return (0);
 }
@@ -52,6 +74,8 @@ int	is_token(int pos, const char *line)
 	if (line[pos] == 0)
 		return (0);
 	else if (passed_shell_operator(pos, line))
+		return (0);
+	else if (passed_redir_sign(pos, line))
 		return (0);
 	else if (!ft_isspace(line[pos]) && ft_isprint(line[pos]))
 		return (1);
