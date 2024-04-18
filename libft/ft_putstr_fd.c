36b4a60ea61f7ab2247a19b6fc8d6573729dc382
ddavlety <ddavlety@student.42berlin.de>
2024-04-18 14:49:51 +0200

    norm libft

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: ddavlety <ddavlety@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:02:33 by ddavlety          #+#    #+#             */
-/*   Updated: 2024/02/21 13:55:44 by ddavlety         ###   ########.fr       */
+/*   Updated: 2024/04/18 14:48:57 by ddavlety         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_putstr_fd(const char  *s, int fd)
+int	ft_putstr_fd(const char *s, int fd)
 {
 	return (write(fd, s, ft_strlen(s)));
 }
