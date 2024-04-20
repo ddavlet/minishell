fb225e12a8b295132e625730ecd0240635cf1e5f
ddavlety <ddavlety@student.42berlin.de>
2024-04-20 11:55:09 +0200

    norm main

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vketteni <vketteni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ddavlety <ddavlety@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:33:41 by vketteni          #+#    #+#             */
-/*   Updated: 2024/04/20 11:53:10 by vketteni         ###   ########.fr       */
+/*   Updated: 2024/04/20 11:54:59 by ddavlety         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_cmd2	*create_cmds(int argc, char *argv[], t_env *shell_env)
 		tokens = tokenizer(prompt);
 		free(prompt);
 	}
-	return(parser(tokens, shell_env));
+	return (parser(tokens, shell_env));
 }
 
 int	main(int argc, char *argv[], const char *envp[])
@@ -50,7 +50,7 @@ int	main(int argc, char *argv[], const char *envp[])
 		g_signal = 0;
 		configure_signals_shell_input();
 		cmds = create_cmds(argc, argv, shell_env);
-		execution(cmds);    
+		execution(cmds);
 		if (is_subshell(argc, argv))
 			terminate_shell(shell_env, EXIT_SUCCESS, NULL);
 	}
