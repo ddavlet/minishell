66a2f834f7234d6c98952b7b84ed33e86ab3dbe9
Vincent Ketteniss <vketteni@c4b4c5.42berlin.de>
2024-04-25 15:44:12 +0200

    norminette

--- execution/execution.c
+++ execution/execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddavlety <ddavlety@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: vketteni <vketteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:32:19 by vketteni          #+#    #+#             */
-/*   Updated: 2024/04/25 15:20:21 by ddavlety         ###   ########.fr       */
+/*   Updated: 2024/04/25 15:43:57 by vketteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,29 @@ void	reset_input_output(int stdin, int stdout)
 		ft_putendl_fd("minishell: failed to reset stdout", STDERR_FILENO);
 }
 
+int	handle_previous_pipe(t_cmd2 *cmd)
+{
+	if (get_previous_cmd(cmd) && get_previous_cmd(cmd)->execution->pipe)
+	{
+		close_fd(get_previous_cmd(cmd)->execution->pipe->read);
+		close_fd(get_previous_cmd(cmd)->execution->pipe->write);
+	}
+	return (1);
+}
+
+void	execute(t_cmd2 *cmd)
+{
+	expand_variables(cmd, cmd->execution->shell_env);
+	if (is_builtin(cmd) && !is_piped(cmd))
+		builtin_router(cmd);
+	else 
+	{
+		execute_cmd(cmd);
+		handle_previous_pipe(cmd);
+		return ;
+	}
+}
+
 void	execution_loop(t_cmd2 *cmds, int stdin, int stdout)
 {
 	t_cmd2	*cmd;
@@ -27,28 +50,16 @@ void	execution_loop(t_cmd2 *cmds, int stdin, int stdout)
 	cmd = cmds;
 	while (cmd)
 	{
-
 		set_input_output(cmd);
 		if (g_signal == SIGINT)
 			break ;
 		if (cmd->execution->argv[0])
-		{
-			expand_variables(cmd, cmds->execution->shell_env);
-			if (is_builtin(cmd) && !is_piped(cmd))
-				builtin_router(cmd);
-			else if (execute_cmd(cmd))
-				return ;
-			if (get_previous_cmd(cmd))
-			{
-				close_fd(get_previous_cmd(cmd)->execution->pipe->read);
-				close_fd(get_previous_cmd(cmd)->execution->pipe->write);
-			}
-			if (cmd->next == NULL || is_logic_operation(cmd))
-				if (wait_check(cmd))
-					break ;
-		}
+			execute(cmd);
 		else
 			cmd->execution->exit_status = EXIT_SUCCESS;
+		if (cmd->next == NULL || is_logic_operation(cmd))
+			if (wait_check(cmd))
+				break ;
 		reset_input_output(stdin, stdout);
 		cmd = cmd->next;
 	}
@@ -57,8 +68,8 @@ void	execution_loop(t_cmd2 *cmds, int stdin, int stdout)
 
 void	execution(t_cmd2 *cmds)
 {
-	int		stdin;
-	int		stdout;
+	int	stdin;
+	int	stdout;
 
 	if (!cmds)
 		return ;
