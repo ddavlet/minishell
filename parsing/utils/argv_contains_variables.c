fc4d9955694a22884a4f4d80013f821f2403935d
Vincent Ketteniss <vketteni@c4c5c3.42berlin.de>
2024-04-26 17:04:59 +0200

    improved find_variable readability

--- parsing/utils/argv_contains_variables.c
+++ parsing/utils/argv_contains_variables.c
@@ -6,7 +6,7 @@
 /*   By: vketteni <vketteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:27:58 by ddavlety          #+#    #+#             */
-/*   Updated: 2024/04/26 14:26:27 by vketteni         ###   ########.fr       */
+/*   Updated: 2024/04/26 17:01:25 by vketteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,20 @@ int	argv_contains_variables(const char **argv)
 	}
 	return (0);
 }
-
-static int	edge_case(const char *literal, const char *dollar_sign)
+static int	is_between_single_quotes(const char *literal,
+		const char *dollar_sign)
 {
 	if (is_between_char(dollar_sign - literal, literal, '\'')
-			&& !is_between_char(ft_strchr(dollar_sign, '\'') - literal, literal, '\"'))
+			&& !is_between_char(ft_strchr(dollar_sign, '\'') - literal, literal,
+		'\"'))
 		return (1);
-	else 
-		return (0);
+	return (0);
+}
+
+static int	dollar_sign_edge_case(const char *literal, const char *dollar_sign)
+{
+	if (is_between_single_quotes(literal, dollar_sign))
+		return (1);
 	if (!ft_isalpha(*(dollar_sign + 1)) && *(dollar_sign + 1) != '?')
 		return (1);
 	if ((literal - dollar_sign != 0) && *(dollar_sign - 1) == '\\')
@@ -45,7 +51,7 @@ const char	*find_variable(const char *literal)
 	const char	*sign;
 
 	sign = ft_strchr(literal, '$');
-	while (sign && edge_case(literal, sign))
+	while (sign && dollar_sign_edge_case(literal, sign))
 		sign = ft_strchr(sign + 1, '$');
 	return (sign);
 }
