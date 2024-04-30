bf3b735c139b3f283812350c1a800aa70eefa5ef
ddavlety <ddavlety@student.42berlin.de>
2024-04-30 11:27:14 +0200

    smal fixe of exit message + signal exit code

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: ddavlety <ddavlety@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:34:47 by vketteni          #+#    #+#             */
-/*   Updated: 2024/04/29 13:36:31 by ddavlety         ###   ########.fr       */
+/*   Updated: 2024/04/30 10:13:18 by ddavlety         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,19 @@ int	builtin_exit(const char **argv, t_cmd2 *cmds, t_env *shell_env)
 	i = 0;
 	while (argv[i])
 		i++;
+	ft_putendl_fd("exit", 1);
 	if (i > 1)
 	{
 		if (!ft_isnumber((char *)argv[1]))
 		{
 			ft_putstr_fd("minshell: exit: ", 2);
 			ft_putstr_fd(argv[1], 2);
-			ft_putstr_fd(": numeric argument required\n", 2);
+			ft_putstr_fd(": numeric argument required", 2);
 			free_exit(2, cmds, shell_env);
 		}
 		if (i > 2)
 		{
-			ft_putstr_fd("exit: too many arguments\n", 2);
+			ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 			return (EXIT_FAILURE);
 		}
 		i = ft_atoi(argv[1]);
